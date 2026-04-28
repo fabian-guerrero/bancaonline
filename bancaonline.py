2cd86ab610fe8970db71e8bc63cf16ae6b5d261c
Mjelectrachapel <mj.ruizredondo1@gmail.com>
2026-04-28 18:41:07 +0200

    Se crea un archivo base

---
+++ bancaonline.py
@@ -0,0 +1,8 @@
+saldo=0
+while True:
+  print(""" 1. Ingresar dinero
+            2. Retirar dinero
+            3. Consultar saldo
+            4. Reiniciar cuenta
+            5. Salir """)
+  opcion = int(input("Introduce una opción:"))
